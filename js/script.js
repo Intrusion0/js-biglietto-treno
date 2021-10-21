e542606ad91a6deddff0499a2840b62d81edbbcf
Mario <lombardomario01@gmail.com>
2021-10-21 15:52:03 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -9,41 +9,41 @@ L’output del prezzo finale va messo fuori in forma umana */
 
 
 // Chiedo all'utente il numero di chilometri che vuole percorrere
-let numChilometri = parseFloat(prompt("Inserisci il numero dei chilometri da percorrere"));
-let numChilometriDecimal = Math.round((numChilometri + Number.EPSILON) * 100) / 100;
-// console.log("N° chilometri da percorrere: ", numChilometriDecimal);
+let n_Kilometres = parseFloat(prompt("Inserisci il numero dei chilometri da percorrere"));
+let n_KilometresDecimal = Math.round((n_Kilometres + Number.EPSILON) * 100) / 100;
+// console.log("N° chilometri da percorrere: ", n_KilometresDecimal);
 
 // Chiedo all'utente la sua età
 let age = parseInt(prompt("Inserisci la tua età"));
 // console.log("La tua età è di: ", age);
 
 // Calcolo prezzo del biglietto in base ai km (0.21 € al km)
-const prezzoBiglietto = 0.21 * numChilometriDecimal;
-const prezzoBigliettoDecimal = Math.round((prezzoBiglietto + Number.EPSILON) * 100) / 100;
-// console.log("Il prezzo del biglietto è di: ", prezzoBigliettoDecimal);
+const ticketPrice = 0.21 * n_KilometresDecimal;
+const ticketPriceDecimal = Math.round((ticketPrice + Number.EPSILON) * 100) / 100;
+// console.log("Il prezzo del biglietto è di: ", ticketPriceDecimal);
 
 // Calcolo prezzo totale del viaggio e seguente Output
-let costo_totale = document.getElementById("costo_totale");
-let percentualeMinorenni;
-let percentualeOverSessantaCinque;
-let prezzoTotale;
-let prezzoTotaleDecimal;
+let total_cost = document.getElementById("total_cost");
+let percentageMinors;
+let percentageOverSixtyFive;
+let totalPrice;
+let totalDecimalPrice;
 
 if (age < 18) {
-    percentualeMinorenni = (prezzoBigliettoDecimal * 20) / 100;
-    prezzoTotale = prezzoBigliettoDecimal - percentualeMinorenni;
-    prezzoTotaleDecimal = Math.round((prezzoTotale + Number.EPSILON) * 100) / 100;
-    costo_totale.innerHTML = ` Il prezzo totale del viaggio è di: <strong>${prezzoTotaleDecimal}€</strong>. <br> 
+    percentageMinors = (ticketPriceDecimal * 20) / 100;
+    totalPrice = ticketPriceDecimal - percentageMinors;
+    totalDecimalPrice = Math.round((totalPrice + Number.EPSILON) * 100) / 100;
+    total_cost.innerHTML = ` Il prezzo totale del viaggio è di: <strong>${totalDecimalPrice}€</strong>. <br> 
     Essendo minorenne hai usufruito del 20% di sconto`;
 } else if (age >= 65) {
-    percentualeOverSessantaCinque = (prezzoBigliettoDecimal * 40) / 100;
-    prezzoTotale = prezzoBigliettoDecimal - percentualeOverSessantaCinque;
-    prezzoTotaleDecimal = Math.round((prezzoTotale + Number.EPSILON) * 100) / 100;
-    costo_totale.innerHTML = ` Il prezzo totale del viaggio è di: <strong>${prezzoTotaleDecimal}€</strong>. <br> 
+    percentageOverSixtyFive = (ticketPriceDecimal * 40) / 100;
+    totalPrice = ticketPriceDecimal - percentageOverSixtyFive;
+    totalDecimalPrice = Math.round((totalPrice + Number.EPSILON) * 100) / 100;
+    total_cost.innerHTML = ` Il prezzo totale del viaggio è di: <strong>${totalDecimalPrice}€</strong>. <br> 
     Essendo over 65 hai usufruito del 40% di sconto `;
 } else {
-    prezzoTotaleDecimal = prezzoBigliettoDecimal;
-    costo_totale.innerHTML = ` Il prezzo totale del viaggio è di: <strong>${prezzoTotaleDecimal}€</strong>.`;
+    totalDecimalPrice = ticketPriceDecimal;
+    total_cost.innerHTML = ` Il prezzo totale del viaggio è di: <strong>${totalDecimalPrice}€</strong>.`;
 }
 
 // console.log("Il prezzo totale del viaggio è di: ", prezzoTotaleDecimal);
